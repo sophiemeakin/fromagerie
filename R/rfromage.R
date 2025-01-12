665e8db615dfdf2db35c5ed7cd90560c9dc7ebfc
sophiemeakin <sophie.meakin@epicentre.msf.org>
2025-01-12 21:22:32 +0100

    Update info function * all variables now in english * report regions instead of deparments

--- R/rfromage.R
+++ R/rfromage.R
@@ -11,28 +11,28 @@ rfromage <- function() {
   
   # get cheese family
   dat_fam <- fromagerie::ref_fromagefam |> 
-    dplyr::filter(fromage_famille == dat$fromage_famille)
+    dplyr::filter(cheese_type == dat$cheese_type)
   # get milk treatment
   dat_milk_trt <- dplyr::case_when(
-    dat$lait_traitment == "cru" ~ "raw",
-    dat$lait_traitment == "pasteurisé" ~ "pasteurised",
-    grepl("cru", dat$lait_traitment) & grepl("pasteurisé", dat$lait_traitment) ~ "raw or pasteurised",
-    grepl("cru", dat$lait_traitment) & grepl("pasteurisé", dat$lait_traitment) ~ "heated or pasteurised",
+    dat$milk_treatment == "cru" ~ "raw",
+    dat$milk_treatment == "pasteurisé" ~ "pasteurised",
+    grepl("cru", dat$milk_treatment) & grepl("pasteurisé", dat$milk_treatment) ~ "raw or pasteurised",
+    grepl("cru", dat$milk_treatment) & grepl("pasteurisé", dat$milk_treatment) ~ "heated or pasteurised",
     TRUE ~ ""
   )
   # get milk type
   dat_milk_ani <- dplyr::case_when(
-    dat$lait_animal == "vache" ~ "cow",
-    dat$lait_animal == "brebis" ~ "sheep",
-    dat$lait_animal == "chèvre" ~ "goat"
+    dat$milk_animal == "vache" ~ "cow",
+    dat$milk_animal == "brebis" ~ "sheep",
+    dat$milk_animal == "chèvre" ~ "goat"
   )
   
   # Generate output
   cat(
-    "Here, try some ", dat$nom, " !",
-    "\n", dat$nom, " is a ", tolower(dat_fam$fromage_famille_en),
+    "Here, try some ", dat$name, " !",
+    "\n", dat$name, " is a ", tolower(dat_fam$cheese_type_en),
     " made from ", dat_milk_trt, " ", dat_milk_ani, "'s milk,",
-    "\n    and is produced in the following departments: ", dat$departements, ".",
+    "\n  and is produced in the following region(s): ", dat$regions, ".",
     sep = ""
   )
   
